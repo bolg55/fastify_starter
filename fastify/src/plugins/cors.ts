9e0839782c1515cfce3bf81493ca28d6d0fedf76
bolg55 <bolger.kellen@gmail.com>
2024-01-12 22:53:28 -0500

    update cors plugin to work w/supertokens

--- fastify/src/plugins/cors.ts
+++ fastify/src/plugins/cors.ts
@@ -1,11 +1,12 @@
 import cors from '@fastify/cors';
 import fastifyPlugin from 'fastify-plugin';
 import { FastifyInstance } from 'fastify';
+import supertokens from 'supertokens-node';
 
 const corsPlugin = async (fastify: FastifyInstance) => {
   await fastify.register(cors, {
     origin: [process.env.API_DOMAIN || 'http://localhost:8080'],
-    allowedHeaders: ['Content-Type', 'Authorization'],
+    allowedHeaders: ['Content-Type', ...supertokens.getAllCORSHeaders()],
     methods: ['GET', 'PUT', 'POST', 'DELETE', 'OPTIONS'],
     credentials: true,
   });
