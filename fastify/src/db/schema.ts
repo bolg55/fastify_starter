6ff86e405220c23b647f74100429ecbeed0cd6bb
bolg55 <bolger.kellen@gmail.com>
2024-02-18 14:14:33 -0500

    update db schema

--- fastify/src/db/schema.ts
+++ fastify/src/db/schema.ts
@@ -5,6 +5,7 @@ import {
   varchar,
   timestamp,
   serial,
+  boolean,
 } from 'drizzle-orm/pg-core';
 
 export const users = pgTable('users', {
@@ -19,8 +20,19 @@ export const profiles = pgTable('profiles', {
   userId: uuid('user_id')
     .primaryKey()
     .references(() => users.id, { onDelete: 'cascade' }),
-  name: varchar('name', { length: 255 }),
+  userName: varchar('user_name', { length: 255 }),
+  createdAt: timestamp('created_at').notNull().defaultNow(),
+  updatedAt: timestamp('updated_at').notNull().defaultNow(),
+});
+
+export const subscriptions = pgTable('subscriptions', {
+  id: serial('id'),
+  userId: uuid('user_id')
+    .primaryKey()
+    .references(() => users.id, { onDelete: 'cascade' }),
   stripeCustomerId: varchar('stripe_customer_id', { length: 255 }),
+  stripeSubscriptionId: varchar('stripe_subscription_id', { length: 255 }),
+  isActive: boolean('is_active').notNull().default(false),
   createdAt: timestamp('created_at').notNull().defaultNow(),
   updatedAt: timestamp('updated_at').notNull().defaultNow(),
 });
