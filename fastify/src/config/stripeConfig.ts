4a00e6ed5d7b7bca42ef25af7e57a911a4f5b825
bolg55 <bolger.kellen@gmail.com>
2024-02-22 23:01:04 -0500

    create Stripe config

--- fastify/src/config/stripeConfig.ts
+++ fastify/src/config/stripeConfig.ts
@@ -4,3 +4,10 @@ import Stripe from 'stripe';
 export const stripe = new Stripe(env.STRIPE_SECRET_KEY, {
   apiVersion: '2023-10-16',
 });
+
+export const getStripeSubTier = async (isActive: boolean, subPlan: string) => {
+  if (isActive) {
+    const sub = await stripe.prices.retrieve(subPlan);
+    return sub.nickname;
+  }
+};
