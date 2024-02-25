f411956529a9c05ff011fd1b8d602775149023ca
bolg55 <bolger.kellen@gmail.com>
2024-02-25 14:53:23 -0500

    restore register transaction

--- fastify/src/services/userServices.ts
+++ fastify/src/services/userServices.ts
@@ -1,21 +1,30 @@
 import { db } from '@db/index';
 import { eq } from 'drizzle-orm';
-import { users } from '@db/schema';
+import { profiles, subscriptions, users } from '@db/schema';
 
 export const createUserAndProfile = async (
   id: string,
   email: string
 ): Promise<void> => {
-  const existingUser = await db.query.users.findFirst({
-    where: eq(users.id, id),
-  });
-
-  if (!existingUser) {
-    await db.insert(users).values({
-      id,
-      email,
+  await db.transaction(async (trx) => {
+    // Check if the user already exists
+    const existingUser = await trx.query.users.findFirst({
+      where: eq(users.id, id),
     });
-  }
+
+    if (!existingUser) {
+      await trx.insert(users).values({
+        id,
+        email,
+      });
+      await trx.insert(profiles).values({
+        userId: id,
+      });
+      await trx.insert(subscriptions).values({
+        userId: id,
+      });
+    }
+  });
 };
 
 export const fetchUser = async (userId: string) => {
