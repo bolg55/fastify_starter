a536ca8f27c3dac88df1e597c52d1c36c09f1b3f
bolg55 <bolger.kellen@gmail.com>
2024-02-25 22:44:40 -0500

    make sub table more robust

--- fastify/src/db/schema.ts
+++ fastify/src/db/schema.ts
@@ -34,6 +34,17 @@ export const profiles = pgTable('profiles', {
   updatedAt: timestamp('updated_at').notNull().defaultNow(),
 });
 
+export const subStatus = pgEnum('sub_status', [
+  'trialing',
+  'active',
+  'canceled',
+  'incomplete',
+  'incomplete_expired',
+  'past_due',
+  'unpaid',
+  'no_subscription',
+]);
+
 export const subscriptions = pgTable('subscriptions', {
   subId: serial('sub_id').primaryKey(),
   userId: uuid('user_id').references(() => users.id, {
@@ -42,8 +53,11 @@ export const subscriptions = pgTable('subscriptions', {
   stripeCustomerId: varchar('stripe_customer_id', { length: 255 }),
   stripeSubscriptionId: varchar('stripe_subscription_id', { length: 255 }),
   isActive: boolean('is_active').notNull().default(false),
+  subStatus: subStatus('sub_status').notNull().default('no_subscription'),
   createdAt: timestamp('created_at').notNull().defaultNow(),
   updatedAt: timestamp('updated_at').notNull().defaultNow(),
+  cancelAtPeriodEnd: boolean('cancel_at_period_end').notNull().default(false),
+  canceledAtDate: timestamp('canceled_at_date'),
 });
 
 export const subscriptionsRelations = relations(subscriptions, ({ one }) => ({
