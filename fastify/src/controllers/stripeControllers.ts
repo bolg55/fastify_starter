27fd26ea6d8ad599d482c98b02ff93a5f770a012
bolg55 <bolger.kellen@gmail.com>
2024-02-26 00:16:30 -0500

    refactor to be cleaner

--- fastify/src/controllers/stripeControllers.ts
+++ fastify/src/controllers/stripeControllers.ts
@@ -1,44 +1,39 @@
 import { env } from '@config/env';
 import { stripe } from '@config/stripeConfig';
 import { FastifyReply, FastifyRequest } from 'fastify';
+
 export const stripeCustomerPortalHandler = async (
   request: FastifyRequest,
   reply: FastifyReply
 ) => {
+  const custId =
+    request.userData?.userProfile.subscriptions[0]?.stripeCustomerId;
+
+  if (!custId) {
+    return reply.code(404).send({
+      status: 'error',
+      message: 'Stripe customer ID not found',
+    });
+  }
+
   try {
-    const custId =
-      request.userData?.userProfile.subscriptions[0].stripeCustomerId;
-
-    console.log('CUST ID:', custId);
-
-    if (!custId) {
-      reply.code(404).send({
-        status: 'error',
-        message: 'Stripe customer ID not found',
-      });
-      return;
-    }
-
     const { url } = await stripe.billingPortal.sessions.create({
       customer: custId,
       return_url: `${env.WEBSITE_DOMAIN}/secret`,
     });
 
-    reply.code(200).send({
+    return reply.code(200).send({
       status: 'success',
       message: 'Billing portal session created successfully',
-      json: { url },
+      url,
     });
   } catch (error) {
-    if (error instanceof Error) {
-      request.log.error(
-        `Error creating billing portal session: ${error.message}`
-      );
-    } else {
-      request.log.error('Unknown error occurred');
-    }
-
-    reply.code(500).send({
+    request.log.error(
+      `Error creating billing portal session: ${
+        error instanceof Error ? error.message : 'Unknown error'
+      }`
+    );
+    return reply.code(500).send({
       status: 'error',
       message: 'Failed to create billing portal session',
     });
