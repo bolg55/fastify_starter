5ab9a230da1e5845e29bd741b01a2c38a58a7a2e
bolg55 <bolger.kellen@gmail.com>
2024-02-27 02:49:01 -0500

    refactor middleware. Keep it DRY

--- fastify/src/middlewares/getUserMiddleware.ts
+++ fastify/src/middlewares/getUserMiddleware.ts
@@ -1,61 +1,31 @@
 import Session from 'supertokens-node/recipe/session';
-import { db } from '@db/index'; // Ensure correct import path
 import { FastifyReply, FastifyRequest } from 'fastify';
-import { users } from '@db/schema';
-import { eq } from 'drizzle-orm';
+import { fetchUser } from 'services/userServices';
 
 const getUserMiddleware = async (
   request: FastifyRequest,
   reply: FastifyReply
 ) => {
-  try {
-    const session = await Session.getSession(request, reply);
-    const userId = session.getUserId();
+  const session = await Session.getSession(request, reply);
+  const userId = session.getUserId();
 
-    let userProfile;
+  let cachedProfile = await request.server.redis.get(`user:${userId}:profile`);
+  let userProfile;
 
-    try {
-      userProfile = await request.server.redis.get(`user:${userId}:profile`);
-    } catch (error) {
-      console.error('Error fetching user profile from cache:', error);
-    }
+  if (!cachedProfile) {
+    userProfile = await fetchUser(userId);
 
-    if (!userProfile) {
-      userProfile = await db.query.users.findFirst({
-        where: eq(users.id, userId),
-        with: {
-          profile: {
-            columns: {
-              userId: true,
-              userName: true,
-            },
-          },
-          subscriptions: {
-            columns: {
-              stripeCustomerId: true,
-              isActive: true,
-              subStatus: true,
-              cancelAtPeriodEnd: true,
-            },
-          },
-        },
-      });
-
-      await request.server.redis.set(
-        `user:${userId}:profile`,
-        JSON.stringify(userProfile),
-        'EX',
-        60 * 10 // Cache for 10 minutes
-      );
-    } else {
-      userProfile = JSON.parse(userProfile);
-    }
-
-    request.userData = { userProfile }; // Use dedicated property for user data
-  } catch (error) {
-    console.error('Error fetching user profile:', error);
-    reply.code(500).send('Internal server error'); // Handle error gracefully
+    await request.server.redis.set(
+      `user:${userId}:profile`,
+      JSON.stringify(userProfile),
+      'EX',
+      60 * 10 // Cache for 10 minutes
+    );
+  } else {
+    userProfile = JSON.parse(cachedProfile);
   }
+
+  request.userData = { userProfile }; // Use dedicated property for user data
 };
 
 export default getUserMiddleware;
