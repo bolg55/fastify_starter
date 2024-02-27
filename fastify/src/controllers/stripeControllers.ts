ef15c8ac9e19a989c99958911070aaef94a6490c
bolg55 <bolger.kellen@gmail.com>
2024-02-27 01:44:07 -0500

    add stripe checkout-session

--- fastify/src/controllers/stripeControllers.ts
+++ fastify/src/controllers/stripeControllers.ts
@@ -1,6 +1,9 @@
 import { env } from '@config/env';
 import { stripe } from '@config/stripeConfig';
 import { FastifyReply, FastifyRequest } from 'fastify';
+import Stripe from 'stripe';
+
+const baseURL = env.WEBSITE_DOMAIN;
 
 export const stripeCustomerPortalHandler = async (
   request: FastifyRequest,
@@ -19,7 +22,7 @@ export const stripeCustomerPortalHandler = async (
   try {
     const { url } = await stripe.billingPortal.sessions.create({
       customer: custId,
-      return_url: `${env.WEBSITE_DOMAIN}/secret`,
+      return_url: `${baseURL}/secret`,
     });
 
     return reply.code(200).send({
@@ -38,4 +41,50 @@ export const stripeCustomerPortalHandler = async (
       message: 'Failed to create billing portal session',
     });
   }
+};
+
+export const stripeCheckoutSessionHandler = async (
+  request: FastifyRequest,
+  reply: FastifyReply
+) => {
+  const { priceId } = request.body as { priceId: string };
+  const userId = request.userData?.userProfile.id;
+
+  const params: Stripe.Checkout.SessionCreateParams = {
+    mode: 'subscription',
+    payment_method_types: ['card'],
+    billing_address_collection: 'auto',
+    line_items: [
+      {
+        price: priceId,
+        quantity: 1,
+      },
+    ],
+    success_url: `${baseURL}/success?session_id={CHECKOUT_SESSION_ID}`,
+    cancel_url: `${baseURL}/cancel`,
+    allow_promotion_codes: true,
+    subscription_data: {
+      trial_period_days: 14,
+      metadata: {
+        payingUserId: userId as string,
+      },
+    },
+  };
+
+  // Handle errors
+  if (!userId) {
+    return reply.code(404).send({
+      status: 'error',
+      message: 'User ID not found',
+    });
+  }
+
+  // Create a new checkout session
+
+  const checkoutSession = await stripe.checkout.sessions.create(params);
+  return reply.code(200).send({
+    status: 'success',
+    message: 'Checkout session created successfully',
+    sessionId: checkoutSession.id,
+  });
 };
--- fastify/src/routes/stripeRoutes.ts
+++ fastify/src/routes/stripeRoutes.ts
@@ -1,17 +1,26 @@
-import { stripeCustomerPortalHandler } from 'controllers/stripeControllers';
+import {
+  stripeCheckoutSessionHandler,
+  stripeCustomerPortalHandler,
+} from 'controllers/stripeControllers';
 import { FastifyInstance } from 'fastify';
 import getUserMiddleware from 'middlewares/getUserMiddleware';
 import { verifySession } from 'supertokens-node/recipe/session/framework/fastify';
 
 const stripeRoutes = async (server: FastifyInstance) => {
-  server.route({
-    method: 'GET',
-    url: '/customer-portal',
+  server.get('/customer-portal', {
     schema: {
       tags: ['Stripe'],
     },
     preHandler: [verifySession(), getUserMiddleware],
     handler: stripeCustomerPortalHandler,
+  });
+
+  server.post('/checkout-session', {
+    schema: {
+      tags: ['Stripe'],
+    },
+    preHandler: [verifySession(), getUserMiddleware],
+    handler: stripeCheckoutSessionHandler,
   });
 };
 
