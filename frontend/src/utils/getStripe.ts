753659d4902920aab379d40c8aead28e7a701e93
bolg55 <bolger.kellen@gmail.com>
2024-03-01 00:54:21 -0500

    Add Stripe util to FE

---
+++ frontend/src/utils/getStripe.ts
@@ -0,0 +1,12 @@
+import { Stripe, loadStripe } from '@stripe/stripe-js';
+
+let stripePromise: Promise<Stripe | null>;
+
+const getStripe = () => {
+  if (!stripePromise) {
+    stripePromise = loadStripe(import.meta.env.VITE_STRIPE_PUBLIC_KEY);
+  }
+  return stripePromise;
+};
+
+export default getStripe;
