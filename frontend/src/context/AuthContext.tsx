f51fc181cc99e6a173f8965b3777d88f0892c305
bolg55 <bolger.kellen@gmail.com>
2024-03-05 13:46:54 -0500

    feat: auth context

---
+++ frontend/src/context/AuthContext.tsx
@@ -0,0 +1,26 @@
+import { ReactNode, createContext, useEffect, useState } from 'react';
+import Session from 'supertokens-web-js/recipe/session';
+
+interface AuthContextType {
+  isLoggedIn: boolean | null;
+}
+
+export const AuthContext = createContext<AuthContextType | null>(null);
+
+export const AuthProvider = ({ children }: { children: ReactNode }) => {
+  const [isLoggedIn, setIsLoggedIn] = useState<boolean | null>(null);
+
+  useEffect(() => {
+    const checkSession = async () => {
+      const sessionExists = await Session.doesSessionExist();
+      setIsLoggedIn(sessionExists);
+    };
+    checkSession();
+  }, []);
+
+  return (
+    <AuthContext.Provider value={{ isLoggedIn }}>
+      {children}
+    </AuthContext.Provider>
+  );
+};
