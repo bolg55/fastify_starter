13349d94b45a5f00555a8eef56d09260eb71ab16
bolg55 <bolger.kellen@gmail.com>
2024-03-04 23:57:21 -0500

    fix: update to use new SDK

--- frontend/src/utils/auth.ts
+++ frontend/src/utils/auth.ts
@@ -1,8 +1,8 @@
 import { QueryClient } from '@tanstack/react-query';
-import { signOut } from 'supertokens-auth-react/recipe/thirdpartypasswordless';
+import Session from 'supertokens-web-js/recipe/session';
 
 export const logout = async (queryClient: QueryClient) => {
-  await signOut();
+  await Session.signOut();
   queryClient.invalidateQueries({ queryKey: ['userProfile'] });
   window.location.href = '/';
 };
