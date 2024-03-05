ffd45adfcf12292853fcbbc9c058cf1f90d0d393
bolg55 <bolger.kellen@gmail.com>
2024-03-05 09:38:49 -0500

    fix: layout

--- frontend/src/Layout.tsx
+++ frontend/src/Layout.tsx
@@ -1,9 +1,9 @@
-import { Outlet } from '@tanstack/react-router';
+import React from 'react';
 
-const Layout = () => {
+const Layout = ({ children }: { children: React.ReactNode }) => {
   return (
-    <div className='flex justify-center pt-16 text-lg bg-slate-800 text-neutral-200'>
-      <Outlet />
+    <div className='flex justify-center h-screen pt-16 text-lg bg-white text-neutral-600'>
+      {children}
     </div>
   );
 };
