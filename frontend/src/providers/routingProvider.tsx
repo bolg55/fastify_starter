11803f03d1a128ca7b6235de8922841f75f4d1b0
bolg55 <bolger.kellen@gmail.com>
2024-03-04 23:51:26 -0500

    chore: remove depricated function

--- frontend/src/providers/routingProvider.tsx
+++ frontend/src/providers/routingProvider.tsx
@@ -1,17 +1,10 @@
-import { Route as rootRoute } from '../routes/__root';
-import { createRouter, NotFoundRoute } from '@tanstack/react-router';
+import { createRouter } from '@tanstack/react-router';
 // Import the generated route tree
 import { routeTree } from '../routeTree.gen';
-
-const notFoundRoute = new NotFoundRoute({
-  getParentRoute: () => rootRoute,
-  component: () => '404 Not Found',
-});
 
 // Create a new router instance
 const router = createRouter({
   routeTree,
-  notFoundRoute,
 });
 
 // Register the router instance for type safety
--- frontend/src/routes/index.lazy.tsx
+++ frontend/src/routes/index.lazy.tsx
@@ -1,14 +1,8 @@
 import { createLazyFileRoute } from '@tanstack/react-router';
 import Home from '../Home';
+import Loader from '../components/Loader';
 
 export const Route = createLazyFileRoute('/')({
+  pendingComponent: Loader,
   component: Home,
 });
-
-// function Index() {
-//   return (
-//     <div className='p-2'>
-//       <h3>Welcome Home!</h3>
-//     </div>
-//   );
-// }
